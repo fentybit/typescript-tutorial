f0b6cc06c899adff694e7b8810291a97589e087a
Hall <fenty.hall@fhall-6107.local>
2021-06-18 15:58:46 -0700

    finish lesson 9

---
+++ lesson-9/src/sandbox.ts
@@ -0,0 +1,12 @@
+//! TYPE ALIASES
+
+type StringOrNum = string | number;
+type objWithName = { name: string; uid: StringOrNum };
+
+const logDetails = (uid: StringOrNum, item: string) => {
+  console.log(`${item} has a uid of ${uid}`);
+};
+
+const greet = (user: objWithName) => {
+  console.log(`${user.name} says hello`);
+};
