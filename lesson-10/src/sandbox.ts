b88b1e6c54bdba13b167b63f81313cc70de72481
Hall <fenty.hall@fhall-6107.local>
2021-06-19 19:54:23 -0700

    finish lesson 10

---
+++ lesson-10/src/sandbox.ts
@@ -0,0 +1,30 @@
+//! FUNCTION SIGNATURES
+
+// let greet: Function;
+
+// example 1
+let greet: (a: string, b: string) => void;
+// you can also define as x and y instead of a and b
+greet = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
+};
+
+// example 2
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+  if (action === "add") {
+    return numOne + numTwo;
+  } else {
+    return numOne - numTwo;
+  }
+};
+
+// example 3
+let logDetails: (obj: { name: string; age: number }) => void;
+
+type person = { name: string; age: number };
+
+logDetails = (ninja: person) => {
+  console.log(`${ninja.name} is ${ninja.age} years old`);
+};
