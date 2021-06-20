1472b389ba6efffd5cc64045f29125cd8e714e1b
Hall <fenty.hall@fhall-6107.local>
2021-06-20 08:53:15 -0700

    finish final lesson

---
+++ lesson-20/src/app.ts
@@ -0,0 +1,52 @@
+//! TUPLES
+// it allows to utilize re-usable codes
+
+import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { Payment } from "./classes/Payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+
+// inputs
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+// list template instance
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  let values: [string, string, number] = [
+    tofrom.value,
+    details.value,
+    amount.valueAsNumber,
+  ];
+
+  let doc: HasFormatter;
+  if (type.value === "invoice") {
+    doc = new Invoice(...values);
+  } else {
+    doc = new Payment(...values);
+  }
+
+  list.render(doc, type.value, "end");
+});
+
+// tuples
+let arr = ["ryu", 25, true];
+arr[0] = false;
+arr[1] = "yoshi";
+arr = [30, false, "yoshi"];
+
+let tup: [string, number, boolean] = ["ryu", 25, true];
+// one each position has been defined, need to follow the hierarchy
+tup[0] = "ken";
+tup[1] = 30;
+
+let student: [string, number];
+student = ["chun-li", 223423];
